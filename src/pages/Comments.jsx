cc642de1e0a13e6068a564c59b1d061d526e02ec
AlexxoK <dmonterroso-2023234@kinal.edu.gt>
2025-05-14 06:32:24 -0600

    getPublications funcional, se avanzó el postComment

---
+++ src/pages/Comments.jsx
@@ -0,0 +1,10 @@
+const Comments = () => {
+    return (
+        <div className="container mt-4">
+            <h2 className="display-6 fw-bold">Section Comments</h2>
+            <p className="text-muted">Here you can read and write comments.</p>
+        </div>
+    );
+};
+
+export default Comments;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,19 @@
-import Blog from "./pages/Blog";
+import Layout from "./components/layout/Layout";
+import Publications from "./pages/Publications";
+import Comments from "./pages/Comments";
+import Courses from "./pages/Courses";
 
 const routes = [
-    {path:'/*', element: <Blog/>}
-]
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            { path: "publications", element: <Publications /> },
+            { path: "comments", element: <Comments /> },
+            { path: "courses", element: <Courses /> },
+            { index: true, element: <Publications /> }, // por defecto va a publications
+        ],
+    },
+];
 
-export default routes
+export default routes;
